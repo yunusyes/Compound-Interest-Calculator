061b6040f191d263551035d0186139a86a9c5ff9
Yunus Yesil <yunusyesil333@gmail.com>
2024-10-04 17:29:27 +0200

    added compound_interest_calculator.py

---
+++ compound_interest_calculator.py
@@ -0,0 +1,21 @@
+class compound_interest_calculator:
+    
+    def __init__(self):
+        self.amount = 0
+        self.interest_rate = 0
+        self.years = 0
+    
+    def calculate(self):
+        self.amount = int(input("Enter the principle amount: "))
+        self.interest_rate = int(input("Enter the interest rate: "))
+        self.years = int(input("Enter the time in years: "))
+        calculated_amount = self.amount*(1 + self.interest_rate/100)**self.years
+        return calculated_amount
+        
+calc = compound_interest_calculator()
+result = calc.calculate()
+print(result)
+
+
+    
+    
